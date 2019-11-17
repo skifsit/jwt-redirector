5936b30e6f3227a010e74c5e3e24c9c54232f6fb
skifsit <skifsit@gmail.com>
2019-11-17 18:46:46 +0200

    Corrected url port generation

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const bundleJS = fs.readFileSync('./dist/bundle.js', 'utf8');
 const port = process.env.PORT || 6776;
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const requestHandler = (req, res) => {
-  if (req.url === '/favicon.ico') {
+  if (req.url.includes('/favicon.ico')) {
     res.setHeader('Content-Type', 'image/x-icon; binary');
     return res.end(favicon);
   }
-  if (req.url === 'dist/bundle.js') {
+  if (req.url.includes('dist/bundle.js')) {
     res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
     return res.end(bundleJS);
   }
